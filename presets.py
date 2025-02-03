736c0d7a712885f57417e0b073c94e039c6e8897
Spencer Couture <spence.couture@gmail.com>
2025-02-03 11:15:12 -0500

    started implementing run functions

--- presets.py
+++ presets.py
@@ -1,3 +1,10 @@
+from topology import Topology
+from certs.certs import create_certs
+from mitmproxy.mitmproxy import get_hostnames
+from dns import start_dnsmasq
+from servers import quiche
+
+
 # the dictionary of name->functions to choose from CLI
 PRESETS = {}
 
@@ -9,6 +16,21 @@ def register_preset(name):
     return wrapper
 
 
-@register_preset("full_run")
-def full_run():
-    print("full run!")
+@register_preset("full_quiche_run")
+def full_quiche_run():
+    quiche_addr = "10.0.9.83"
+    # run full testbed for each of the sites
+    sites = [("www.unh.edu","unh")]
+    for (site, nsid) in sites:
+        # first bring the topo up
+        topo = Topology(nsid).up()
+
+        # get certs and DNS configured
+        hostnames = get_hostnames(site)
+        cert_dir = create_certs(hostnames)
+        start_dnsmasq(topo, hostnames, quiche_addr)
+
+        # start quiche
+        quiche.start(topo, site, quiche_addr, 443, cert_dir)
+
+
